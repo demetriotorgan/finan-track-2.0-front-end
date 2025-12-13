e8b1212bf5a27f434142093df8b960e67c6d5dff
demetriotorgan <demetriotorgan@hotmail.com>
2025-12-12 21:10:50 -0300

    graficoPizza

--- src/Components/GraficoPizza.jsx
+++ src/Components/GraficoPizza.jsx
@@ -1,40 +1,46 @@
-import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
+import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import '../Styles/GraficoPizza.css'
 
-
-const cores = ['#2d455eff', '#9af087ff'];
+const cores = ['#2d455e', '#9af087'];
 
 export default function GraficoPizza({ titulo, data }) {
   return (
-    <div className="grafico-container">
-      <h3>{titulo}</h3>
-      <PieChart width={300} height={300}>
-        <Pie
-          data={data}
-          cx="50%"
-          cy="50%"
-          outerRadius={100}
-          dataKey="value"
-          label={({ value }) =>
-            `Valor: ${new Intl.NumberFormat('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(value)}`
-          }
-        >
-          {data.map((_, index) => (
-            <Cell key={index} fill={cores[index % cores.length]} />
-          ))}
-        </Pie>
-        <Tooltip
-          formatter={(value) =>
-            new Intl.NumberFormat('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(value)
-          }
-        />
-        <Legend />
-      </PieChart>
+    <div className="grafico-pizza-card">
+      <h3 className="grafico-pizza-titulo">{titulo}</h3>
+
+      <div className="grafico-pizza-box">
+        <ResponsiveContainer width="100%" height={260}>
+          <PieChart>
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              outerRadius={90}
+              dataKey="value"
+              label={({ value }) =>
+                new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(value)
+              }
+            >
+              {data.map((_, index) => (
+                <Cell key={index} fill={cores[index % cores.length]} />
+              ))}
+            </Pie>
+
+            <Tooltip
+              formatter={(value) =>
+                new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(value)
+              }
+            />
+            <Legend />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
