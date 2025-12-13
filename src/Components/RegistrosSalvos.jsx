8514f64a94404cd3f5ba8f4f0d60de643602bdb2
demetriotorgan <demetriotorgan@hotmail.com>
2025-12-13 17:45:29 -0300

    componenteRegistrosFinal

--- src/Components/RegistrosSalvos.jsx
+++ src/Components/RegistrosSalvos.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { useCarregarRegistros } from '../Hooks/useCarregarRegistros'
 import Carregando from './Carregando';
-import { Calendar, DollarSign, FileText, Tag, ArrowDownUp, AlertCircle, Pencil, HandCoins } from 'lucide-react';
+import { Calendar, DollarSign, FileText, Tag, ArrowDownUp, AlertCircle, Pencil, HandCoins, ArrowBigLeft } from 'lucide-react';
 import { useExcluirRegistro } from '../Hooks/useExcluirRegistro';
 import ModalCarregando from '../Components/ModalCarregando';
+import { useNavigate } from 'react-router-dom';
 
 const RegistrosSalvos = () => {
   const { carregarRegistros, loading, carregarUltimosRegistros } = useCarregarRegistros();
-  const { excluindo, excluirRegistro } = useExcluirRegistro({ carregarUltimosRegistros })
+  const { excluindo, excluirRegistro } = useExcluirRegistro({ carregarUltimosRegistros });
+   const navigate = useNavigate();  
+   const handleVoltar = () => {
+    navigate('/')
+  }
 
   return (
-    <div className='container'>
+    <div className='container'>      
       <h3><HandCoins /> Todos os Lançamentos</h3>
+      <button className='btn btn-editar' onClick={handleVoltar}><ArrowBigLeft /> Voltar</button>
+
       {excluindo && <ModalCarregando label='Exclundo' />}
       {loading && <Carregando label='Carregando...' />}
 
@@ -66,10 +73,9 @@ const RegistrosSalvos = () => {
               onClick={() => excluirRegistro(registro)}>
               🗑️ Excluir
             </button>
-
           </div>
-        ))}
-      </div>
+        ))}        
+      </div>      
     </div>
   )
 }
