f402f22cb4b32a346fdfdf3dc327122651bd9647
demetriotorgan <demetriotorgan@hotmail.com>
2025-12-15 11:56:29 -0300

    salvarObjetivo

--- src/Components/Objetivos.jsx
+++ src/Components/Objetivos.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dateToIso, isoToDateEdit } from '../utils/time';
+import api from '../api/api';
+import { useSalvarObjetivo } from '../Hooks/useSalvarObjetivo';
 
 const Objetivos = () => {
+    const {dadosObjetivo, handleDados, salvarObjetivo} = useSalvarObjetivo();
+
   return (
-    <div>
-        Objetivos
+    <div className='container'>
+      <h2>Cadastrar Objetivos</h2>
+      <div className='form-registro'>
+        <label>
+        Descrição
+        <input 
+        type='text'
+        name='descricao'
+        value={dadosObjetivo.descricao}
+        onChange={handleDados}
+        />
+      </label>
+      <label>
+        Limite
+        <input 
+        type='number'
+        name='limite'
+        value={dadosObjetivo.limite}
+        onChange={handleDados}
+        />
+      </label>
+      <label>
+        Categoria
+        <select name='categoria' value={dadosObjetivo.categoria} onChange={handleDados}>
+          <option value="supermercado">Supermercado</option>
+          <option value="bebidas">Bebidas</option>
+          <option value="lanche">Lanche</option>
+          <option value="abastecimento">Abastecimento</option>
+          <option value="agua">Água</option>
+          <option value="luz">Luz</option>
+          <option value="internet">Internet</option>
+          <option value="farmacia">Farmácia</option>
+          <option value="outro">Outros</option>
+        </select>
+      </label>
+      <label>
+        Período
+        <select name='periodo' value={dadosObjetivo.periodo} onChange={handleDados}>
+          <option value="1">Janeiro</option>
+          <option value="2">Feveiro</option>
+          <option value="3">Março</option>
+          <option value="4">Abril</option>
+          <option value="5">Maio</option>
+          <option value="6">Junho</option>
+          <option value="7">Julho</option>
+          <option value="8">Agosto</option>
+          <option value="9">Setembro</option>
+          <option value="10">Outubro</option>
+          <option value="11">Novembro</option>
+          <option value="12">Dezembro</option>
+        </select>
+      </label>
+      <label>
+        Data
+        <input 
+        type='date'
+        name='data'
+        value={dadosObjetivo.data}
+        onChange={handleDados}
+        />
+      </label>
+      <button className='btn btn-salvar' onClick={salvarObjetivo}>Salvar</button>
+      </div>     
     </div>
   )
 }
---
+++ src/Hooks/useSalvarObjetivo.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import api from "../api/api";
+import { dateToIso, isoToDateEdit } from "../utils/time";
+
+
+export function useSalvarObjetivo() {
+    const hojeISO = new Date().toISOString();
+
+    const objetivoInicial = {
+        descricao: '',
+        limite: '',
+        categoria: 'supermercado',
+        periodo: '1',
+        data: isoToDateEdit(hojeISO)
+    };
+
+    const [dadosObjetivo, setDadosObjetivo] = useState(objetivoInicial);
+    const [salvandoObjetivo, setSalvandoObjetivo] = useState(false);
+
+    const handleDados = (e) => {
+        const { name, value } = e.target
+        setDadosObjetivo({ ...dadosObjetivo, [name]: value });
+    };
+
+    const salvarObjetivo = async () => {
+        const payload = {
+            descricao: dadosObjetivo.descricao,
+            limite: dadosObjetivo.limite,
+            categoria: dadosObjetivo.categoria,
+            periodo: dadosObjetivo.periodo,
+            data: dateToIso(dadosObjetivo.data)
+        }
+
+        const confirmar = window.confirm('Deseja salvar este objetivo?');
+        if (!confirmar) return
+
+        try {
+            setSalvandoObjetivo(true);
+            const response = await api.post('/salvar-objetivo', payload);
+            console.log(response.data);
+            alert('Objetivo salvo com sucesso');
+            setDadosObjetivo(objetivoInicial);
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setSalvandoObjetivo(false);
+        }
+    };
+
+    return {
+        dadosObjetivo,
+        handleDados,
+        salvarObjetivo
+    }
+}
